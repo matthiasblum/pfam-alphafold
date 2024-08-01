4949e791f4e5eae3941c04d9a43974dea6e4be97
Matthias Blum <mat.blum@gmail.com>
2024-08-01 12:23:26 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
    name="pfam-alphafold",
-   version="1.0.0",
+   version="1.0.1",
    author="Matthias Blum",
    author_email="mblum@ebi.ac.uk",
    description="Build a SQLite3 database incorporating Pfam families and "
